3c6c56eea0c0900f5b54903daacae59a4903b908
Daniel <daniello130@o2.pl>
2015-02-24 10:05:39 +0100

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Daniel on 2/23/15.
+//  Copyright (c) 2015 Daniel Stradowski Codespinning. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
